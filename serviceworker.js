b4cdc70caa10ecaa98639b23cac0f5d0e960c8ee
Eniko Valko <eaaenva@students.eaaa.dk>
2022-10-05 09:29:11 +0200

    complete new files

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,31 @@
-self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+//Giving the cache a foldername
+const cacheName = "cache-insects";
+
+//When the browser reads this for the first time, it caches all the files mentioned in the list.
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll([
+        "/insects/",
+        "/insects/index.html",
+        "/insects/butterfly.jpg/",
+        "/insects/butterflies.jpg",
+        "/insects/dragonfly.jpg",
+      ]);
+    })
+  );
 });
+
+//If the user requests resource 
+// (file, html, css, image, css etc.)Then look 
+// for it onnline. If not available online, it 
+// gets the file from the cache
+
+self.addEventListener("fetch", function(event){
+    event.respondWith(
+        //The catch is for error handling.
+        fetch(event.request).catch(() =>
+        caches.open(cacheName).then(cache => cache.match(event.request))
+        )
+    )
+})
